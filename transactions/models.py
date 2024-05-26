41b65fd89aeaf5c2ea32c0a3070bdcef249e9ade
Nikolas Rueckmann <nkls99@gmail.com>
2024-05-26 13:49:03 -0300

    changes on models and migrations

--- transactions/models.py
+++ transactions/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-
-class TransactionCategory(models.Model):
-  description = models.CharField(max_length=255)
+from categories.models import Category
 
 class Transaction(models.Model):
   description = models.CharField(max_length=255)
   value = models.DecimalField(max_digits=10, decimal_places=2)
   type = models.CharField(max_length=6)
   date = models.DateTimeField()
-  category_id = models.ForeignKey(TransactionCategory, on_delete=models.CASCADE)
+  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
