903c4185f6e1eb1c6ec89aeafce9fa2b7d970115
Nikolas Rueckmann <nkls99@gmail.com>
2024-05-26 19:10:14 -0300

    Implements repo and serializer on listing route

--- categories/views.py
+++ categories/views.py
@@ -1,11 +1,18 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.request import Request
+from .repository import CategoryRepository
+from .serializers import CategorySerializer
 
 class CategoriesView(ViewSet):
+  repo = CategoryRepository()
+  
   def list(self, request: Request):
     return Response(
-      "rota de listagem de categorias"
+      map(
+        lambda category: CategorySerializer(category).data,
+        self.repo.getAll()
+      )
     )
   
   def retrieve(self, request: Request, pk: int):
